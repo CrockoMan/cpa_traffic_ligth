f56f1e90b8a43a5bcc78ec4d597f418f77e3e9ac
Константин Гурашкин <crocko@mail.ru>
2024-06-28 17:02:35 +0300

    Update fill.bd

--- person/management/commands/fill_bd.py
+++ person/management/commands/fill_bd.py
@@ -5,9 +5,9 @@ from decimal import Decimal
 import random
 from progress.bar import IncrementalBar
 
-from person.models import Department, Employee
+from person.models import Department, Employee, Position
 
-
+POSITION_COUNT = 50
 FILL_COUNT = 50000
 
 fake = Faker()
@@ -24,9 +24,8 @@ def create_department(parent=None, level=0):
         create_department(parent=department, level=level+1)
 
 
-def create_employee(department):
+def create_employee(department, position):
     full_name = fake.name()
-    position = fake.job()
     hire_date = fake.date_between(start_date='-10y', end_date='today')
     salary = Decimal(random.randint(30000, 100000))
     Employee.objects.create(
@@ -45,17 +44,25 @@ class Command(BaseCommand):
         with transaction.atomic():
             Department.objects.all().delete()
             Employee.objects.all().delete()
+            Position.objects.all().delete()
 
             root_department = Department.objects.create(name='Headquarters')
             create_department(parent=root_department)
 
             departments = Department.objects.all()
+            position = Position.objects.all()
 
-            bar = IncrementalBar('Заполнено', max=FILL_COUNT)
+            bar = IncrementalBar('Заполнение', max=POSITION_COUNT + FILL_COUNT)
+            for _ in range(POSITION_COUNT):
+                bar.next()
+                Position.objects.create(position=fake.job())
+
             for _ in range(FILL_COUNT):
                 bar.next()
-                department = random.choice(departments)
-                create_employee(department)
+                create_employee(
+                    random.choice(departments),
+                    random.choice(position)
+                )
             bar.finish()
 
         self.stdout.write(
